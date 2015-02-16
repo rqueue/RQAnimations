7835ed5f16e937a6fcc2cce637003931edf321bd
Ryan Quan <ryanhquan@gmail.com>
2015-02-15 16:17:11 -0800

    Initial Commit

--- RQAnimations/RQAnimations.h
+++ RQAnimations/RQAnimations.h
@@ -1,11 +1,3 @@
-//
-//  RQAnimations.h
-//  RQAnimations
-//
-//  Created by Ryan Quan on 2/15/15.
-//  Copyright (c) 2015 Ryan Quan. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface RQAnimations : NSObject
--- RQAnimations/RQAnimations.m
+++ RQAnimations/RQAnimations.m
@@ -1,11 +1,3 @@
-//
-//  RQAnimations.m
-//  RQAnimations
-//
-//  Created by Ryan Quan on 2/15/15.
-//  Copyright (c) 2015 Ryan Quan. All rights reserved.
-//
-
 #import "RQAnimations.h"
 
 @implementation RQAnimations
